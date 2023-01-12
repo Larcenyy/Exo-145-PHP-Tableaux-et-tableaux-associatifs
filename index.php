0a9fa1f00f22607b6c861ea288f02ee67a836df1
Rémy <remyc59440@hotmail.com>
2023-01-12 11:24:10 +0100

    daa

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
-
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits);
+echo $fruits[1];
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " . $age['Ben'] . " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" .  $y;
 }
 
 
@@ -21,7 +21,9 @@ echo "<br><br>Ben is " .  . " years old.<br><br>";
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
+sort($colors);
+rsort($colors);
+asort($colors);
 
 
 // Quatrieme ligne
